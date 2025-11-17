45ebe90399c9ad6a9a3ae1dc19b48a5f0f87873d
NandovdK <njvdkant@gmail.com>
2025-11-17 22:18:58 +0100

    fix(neovim): show lsp custom signs also when not showing diagnostics text

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -119,9 +119,18 @@ return {
 
 			local function hide_diagnostics()
 				vim.diagnostic.config({
+					float = { border = "rounded", source = "if_many" },
+					underline = { severity = vim.diagnostic.severity.ERROR },
+					signs = vim.g.have_nerd_font and {
+						text = {
+							[vim.diagnostic.severity.ERROR] = "󰅚 ",
+							[vim.diagnostic.severity.WARN] = "󰀪 ",
+							[vim.diagnostic.severity.INFO] = "󰋽 ",
+							[vim.diagnostic.severity.HINT] = "󰌶 ",
+						},
+					},
+
 					virtual_text = false,
-					signs = true,
-					underline = false,
 				})
 			end
 
