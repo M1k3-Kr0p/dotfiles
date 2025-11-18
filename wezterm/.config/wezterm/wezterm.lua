9a356e01d0796d4d35007199479bf410ac8b5276
NandovdK <njvdkant@gmail.com>
2025-11-18 19:45:06 +0100

    feat(wezterm): configure simple multiplexer keybinds

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,5 +1,7 @@
-local wezterm = require("wezterm")
-local config = wezterm.config_builder()
+local w = require("wezterm")
+
+local smart_splits = w.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
+local config = w.config_builder()
 
 function getOS()
 	local sep = package.config:sub(1, 1)
@@ -23,7 +25,7 @@ end
 local os_name = getOS()
 
 config.color_scheme = "Batman"
-config.font = wezterm.font("JetBrains Mono")
+config.font = w.font("JetBrains Mono")
 config.window_padding = {
 	left = 3,
 	right = 3,
@@ -36,16 +38,47 @@ config.tab_bar_at_bottom = false
 config.show_new_tab_button_in_tab_bar = true
 
 config.window_decorations = "RESIZE"
--- config.window_background_opacity = 0.9
 
 config.cursor_blink_rate = 800
 config.default_cursor_style = "SteadyBlock"
 config.max_fps = 120
 
--- windows overwrites
+-- Windows overwrites
 if os_name == "Windows" then
 	config.default_domain = "WSL:Ubuntu"
 	config.hide_tab_bar_if_only_one_tab = false
 end
+
+-- Keymaps
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+	{
+		mods = "LEADER",
+		key = "-",
+		action = w.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "\\",
+		action = w.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "m",
+		action = w.action.TogglePaneZoomState,
+	},
+	{
+		mods = "LEADER",
+		key = "Space",
+		action = w.action.RotatePanes("Clockwise"),
+	},
+	{
+		key = "Enter",
+		mods = "LEADER",
+		action = w.action.ActivateCopyMode,
+	},
+}
+
+smart_splits.apply_to_config(config)
 
 return config
