892dd3fc2e64173db210f2448b74927f022b606c
NandovdK <njvdkant@gmail.com>
2025-11-19 23:33:06 +0100

    fix(wezterm): change underline thickness

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -30,9 +30,21 @@ config.window_padding = {
 	left = 3,
 	right = 3,
 	top = 3,
-	bottom = 0,
+	bottom = 1,
 }
-config.hide_tab_bar_if_only_one_tab = true
+config.window_frame = {
+	inactive_titlebar_bg = "#353535",
+	active_titlebar_bg = "#2b2042",
+	inactive_titlebar_fg = "#cccccc",
+	active_titlebar_fg = "#ffffff",
+	inactive_titlebar_border_bottom = "#2b2042",
+	active_titlebar_border_bottom = "#2b2042",
+	button_fg = "#cccccc",
+	button_bg = "#2b2042",
+	button_hover_fg = "#ffffff",
+	button_hover_bg = "#3b3052",
+}
+config.hide_tab_bar_if_only_one_tab = false
 config.use_fancy_tab_bar = true
 config.tab_bar_at_bottom = false
 config.show_new_tab_button_in_tab_bar = true
@@ -41,16 +53,16 @@ config.inactive_pane_hsb = {
 	brightness = 0.8,
 }
 
+config.underline_thickness = "5px"
 config.window_decorations = "RESIZE"
 
 config.cursor_blink_rate = 800
 config.default_cursor_style = "SteadyBlock"
-config.max_fps = 60
+config.max_fps = 120
 
 -- Windows overwrites
 if os_name == "Windows" then
 	config.default_domain = "WSL:Ubuntu"
-	config.hide_tab_bar_if_only_one_tab = false
 end
 
 -- Keymaps
